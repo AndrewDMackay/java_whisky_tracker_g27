21e0419ae785b939c7e1ad734db37519f8cef3f6
Andrew Mackay <andrewmackayd@gmail.com>
2021-11-24 15:33:03 +0000

    AM, 2nd commit main

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -1,9 +1,28 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class WhiskyController {
 
+    @Autowired
+    WhiskyRepository whiskyRepository;
+
+    @GetMapping(value = "/whiskies")
+    public ResponseEntity<List<Whisky>> findWhiskyFilterByYear(@RequestParam(name = "year", required = false) int year){
+        return new ResponseEntity<>(whiskyRepository.findWhiskyByYear(year), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/whiskies")
+    public ResponseEntity<List<Whisky>> findWhiskiesByAgeAndDistilleryName(@RequestParam(name = "age", required = false) int age, @RequestParam(name = "name", required = false) String name){
+        return new ResponseEntity<>(whiskyRepository.findWhiskiesByAgeAndDistilleryName(age, name), HttpStatus.OK);
+    }
 }
--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository.java
@@ -2,6 +2,14 @@ package com.codeclan.example.WhiskyTracker.repositories;
 
 import com.codeclan.example.WhiskyTracker.models.Whisky;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface WhiskyRepository extends JpaRepository<Whisky, Long> {
+
+    List<Whisky> findWhiskyByYear(int year);
+
+    List<Whisky> findWhiskiesByAgeAndDistilleryName(int age, String name);
 }
